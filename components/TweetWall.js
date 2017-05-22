d9bf1376f90326b363e8ce001d458487d32b59fc
markdegroat <mhdegroat@gmail.com>
2017-05-22 14:57:55 -0400

    Final

--- components/TweetWall.js
+++ components/TweetWall.js
@@ -10,10 +10,28 @@ export default class TweetWall extends React.Component {
   }
 
   // TODO: componentWillMount()
+  componentWillMount(){
+    this.setState({
+      tweets: this.props.newTweets
+    })
+  }
 
   // TODO: shouldComponentUpdate()
+  shouldComponentUpdate(nextProps, nextState){
+    if(nextProps.newTweets.length > 0){
+      return true
+    }else{
+      return false
+    }
+
+  }
 
   // TODO: componentWillReceiveProps()
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      tweets: nextProps.newTweets.concat(this.state.tweets)
+    })
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => {
